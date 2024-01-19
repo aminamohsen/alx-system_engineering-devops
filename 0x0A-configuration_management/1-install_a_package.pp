7dfadfa5e813e4962c05129e35b21cc9d100fb5a
aminamohsen <amina.mohsen4004@gmail.com>
2024-01-19 21:55:34 +0200

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,6 @@
 #!/usr/bin/pup
-#Ensure python3-pip is installed
-package {'python3-pip':
- ensure => installed,
-}
-
+# Install an especific version of flask (2.1.0)
 package {'flask':
- ensre => '2.1.0',
- provider => 'pip',
- require => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-package {'werkzeug':
- ensure => '2.0.1',
- provider => 'pip',
- require => Package['python3-pip'],
-}
-
-
-
-
-
-
-
-
