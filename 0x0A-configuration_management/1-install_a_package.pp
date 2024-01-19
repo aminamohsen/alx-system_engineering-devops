933df7ece9b8bacfc736b1ec59aa2826b350f6a3
aminamohsen <amina.mohsen4004@gmail.com>
2024-01-19 20:24:27 +0200

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Instalar un paquete
+
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  name     => 'puppet-lint',
+  provider => 'gem',
+  source   => 'http://rubygems.org',
 }
---
+++ 0x0A-configuration_management/site.pp
@@ -0,0 +1,13 @@
+# site.pp
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  refreshonly => true,
+}
+
+
