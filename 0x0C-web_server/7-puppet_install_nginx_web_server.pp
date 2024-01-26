0eae760105ea4674d89812cef51661c472899839
aminamohsen <amina.mohsen4004@gmail.com>
2024-01-26 23:54:04 +0200

    web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,49 +1,21 @@
-#!/usr/bin/env bash
-# Configure your Nginx server to have a custom 404 page
+# Automating project requirements using Puppet
 
-apt-get update
-apt-get install -y nginx
-
-mkdir /etc/nginx/html
-touch /etc/nginx/html/index.html
-echo "Hello World!" > /etc/nginx/html/index.html
-printf %s "server {
-     listen      80 default_server;
-     listen      [::]:80 default_server;
-     root        /etc/nginx/html;
-     index       index.html index.htm;
+package { 'nginx':
+  ensure => installed,
 }
-" > /etc/nginx/sites-available/default
 
-touch /etc/nginx/html/404.html
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
+}
 
-echo "Ceci n'est pas une page" | sudo tee /etc/nginx/html/404.html
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
 
-echo "Nginx installation and configuration completed."
-
-echo "server {
-    listen      80;
-    listen      [::]:80;
-    root        /etc/nginx/html;
-    index       index.html index.htm;
-    
-    index index.html index.htm index.nginx-debian.html;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /etc/nginx/html;
-      internal;
-    }
-
-}" | sudo tee /etc/nginx/sites-available/default
-
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
-
-# Test nginx configuration and reload
-sudo nginx -t
-sudo service nginx reload
-
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
