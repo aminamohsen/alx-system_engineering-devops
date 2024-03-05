80bbb9fc242e2d80c90b40328a3da85d43f62e16
aminamohsen <amina.mohsen4004@gmail.com>
2024-03-05 12:55:47 +0200

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+returns the number of subscribers (not active users, total subscribers)
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
