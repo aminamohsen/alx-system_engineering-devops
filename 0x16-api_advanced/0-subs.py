714beac293a20cb4bed1e88f9fc5aa7c5aa6c485
aminamohsen <amina.mohsen4004@gmail.com>
2024-03-05 18:07:53 +0200

    api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
-"""
-this doc for module
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    except (KeyError, requests.RequestException):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-# Example usage:
-subreddit = 'learnpython'
-print(f"Number of subscribers in r/{subreddit}: {number_of_subscribers(subreddit)}")
-
-
-
-
-
-
-
-
+    results = response.json().get("data")
+    return results.get("subscribers")
